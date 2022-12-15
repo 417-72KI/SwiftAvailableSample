37a71ac458946792fd935361bc04430a89ac9d1c
417-72KI <417.72ki@gmail.com>
2022-12-16 01:59:39 +0900

    make half-sheet

--- Sources/AvailableSample/ContentView.swift
+++ Sources/AvailableSample/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             }
             .sheet(isPresented: $isPresenting) {
                 SheetView()
+                    .presentationDetents([.medium])
             }
         }
     }
