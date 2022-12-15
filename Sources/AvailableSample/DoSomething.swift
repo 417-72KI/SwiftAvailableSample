8f9457a3c458b8f534f310452df4eaa45e68256c
417-72KI <417.72ki@gmail.com>
2022-12-16 01:39:50 +0900

    add deprecation

--- Sources/AvailableSample/DoSomething.swift
+++ Sources/AvailableSample/DoSomething.swift
@@ -4,7 +4,12 @@ func doSomesing() {
     // do something
 }
 
+@available(*, deprecated, renamed: "doSomethingWithFoo(_:)")
 func doSomething(with foo: Foo = .init()) {
+    doSomethingWithFoo(foo)
+}
+
+func doSomethingWithFoo(_ foo: Foo = .init()) {
     // do something with `foo`
 }
 
