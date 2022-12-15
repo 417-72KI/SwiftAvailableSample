f92e66cf71d546d50b3b3d14a69a59c859113e94
417-72KI <417.72ki@gmail.com>
2022-12-16 01:39:50 +0900

    add files

---
+++ Sources/AvailableSample/CallingDoSomething.swift
@@ -0,0 +1,39 @@
+func call1() {
+    doSomesing()
+}
+
+func call2() {
+    doSomesing()
+}
+
+func call3() {
+    doSomesing()
+}
+
+func call4() {
+    doSomesing()
+}
+
+func call5() {
+    doSomesing()
+}
+
+func call6() {
+    doSomesing()
+}
+
+func call7() {
+    doSomesing()
+}
+
+func call8() {
+    doSomesing()
+}
+
+func notCall1() {
+    doSomething()
+}
+
+func notCall2() {
+    doSomething()
+}
