c42f500b65a2419ab3d6b7b99c6f586d91cfbafe
417-72KI <417.72ki@gmail.com>
2022-12-16 01:55:14 +0900

    add `obsoleted`

--- Sources/AvailableSample/HalfSheet.swift
+++ Sources/AvailableSample/HalfSheet.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-@available(iOS, deprecated: 16, message: "Use `presentationDetents` directly.")
+@available(iOS, deprecated: 16, obsoleted: 17, message: "Use `presentationDetents` directly.")
 extension View {
     /// iOS16以上はハーフモーダルが適用される。iOS15未満は通常の `pageSheet` になる
     /// - Parameter height: ハーフモーダルの高さ。`nil` の場合画面の半分になる。
