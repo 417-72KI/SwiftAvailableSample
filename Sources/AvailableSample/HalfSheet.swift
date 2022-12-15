792d4d38904f9e583dab622872c1c5450130ac7f
417-72KI <417.72ki@gmail.com>
2022-12-16 01:46:49 +0900

    make half-sheet extension

---
+++ Sources/AvailableSample/HalfSheet.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+extension View {
+    /// iOS16以上はハーフモーダルが適用される。iOS15未満は通常の `pageSheet` になる
+    /// - Parameter height: ハーフモーダルの高さ。`nil` の場合画面の半分になる。
+    @ViewBuilder
+    func halfSheetIfAble(height: CGFloat? = nil) -> some View {
+        if #available(iOS 16.0, *) {
+            if let height {
+                presentationDetents([.height(height)])
+            } else {
+                presentationDetents([.medium])
+            }
+        } else {
+            self
+        }
+    }
+}
