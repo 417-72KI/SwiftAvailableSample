c1319afce77e822a9cdf911e7a4d0b70f90cca1b
417-72KI <417.72ki@gmail.com>
2022-12-16 01:27:01 +0900

    Bump deployment target to iOS 15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AvailableSample",
-    platforms: [.macOS(.v12), .iOS(.v14)],
+    platforms: [.macOS(.v12), .iOS(.v15)],
     products: [
         .library(
             name: "AvailableSample",
