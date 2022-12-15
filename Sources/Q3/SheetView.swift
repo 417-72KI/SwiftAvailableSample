26c8e584489a73514f33ba1ad908106848c41054
417-72KI <417.72ki@gmail.com>
2022-12-16 01:38:50 +0900

    remove deprecations

--- Sources/Q3/SheetView.swift
+++ Sources/Q3/SheetView.swift
@@ -3,16 +3,7 @@ import SwiftUI
 public struct SheetView: View {
     @Environment(\.dismiss) var dismiss
 
-    @available(iOS, deprecated: 15.0, message: "Use dismissAction instead.")
-    @Binding var isPresenting: Bool
-
-    @available(iOS, deprecated: 15.0, message: "`isPresenting` is no longer used. Use `init()`.")
-    public init(isPresenting: Binding<Bool>) {
-        _isPresenting = isPresenting
-    }
-
     public init() {
-        _isPresenting = .constant(false)
     }
 
     public var body: some View {
